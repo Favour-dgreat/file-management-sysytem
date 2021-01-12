8c95c683cfcac7bc88e2cfa253408815bb799a36
yhoungdev <yhoungdev@gmail.com>
2021-01-12 15:41:58 +0000

    its a priviledge sir

--- script.js
+++ script.js
@@ -12,26 +12,26 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   console.log(firebase)
 
-  function uploadImage(){
-      const ref = firebase.storage().ref()
+  //function uploadImage(){
+//       const ref = firebase.storage().ref()
 
-      const file = document.querySelector("#photo").files[0]
+//       const file = document.querySelector("#photo").files[0]
 
-      const name= new Date () + '–' + file.name
+//       const name= new Date () + '–' + file.name
       
-      const metadata = {
-          contentType:file.type
-      }
+//       const metadata = {
+//           contentType:file.type
+//       }
 
-      const task = ref.child(name).put(file,metadata)
+//       const task = ref.child(name).put(file,metadata)
 
-      task
-      .then(snapshot => snapshot.ref.getDownloadURL())
-      .then(url => {
-          console.log(url)
-          alert("Image Upload Successful")
-          const image = document.querySelector('#image')
-          image.src = url       
-        })
-  }
-var button = ('#submit')
+//       task
+//       .then(snapshot => snapshot.ref.getDownloadURL())
+//       .then(url => {
+//           console.log(url)
+//           alert("Image Upload Successful")
+//           const image = document.querySelector('#image')
+//           image.src = url       
+//         })
+//   }
+// var button = ('#submit')
---
+++ styles/readme.md
@@ -0,0 +1,2 @@
+##### sir i cant test it , cause its not my account 
+###### please confirm it sir 
